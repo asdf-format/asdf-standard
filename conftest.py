cdd001c652b21436dd571681f1d8c36300f70ca7
Ed Slavich <eslavich@stsci.edu>
2020-03-05 11:53:08 -0500

    Ignore unrecognized tag warnings in schema tests in this repo

--- conftest.py
+++ conftest.py
@@ -1,19 +1,29 @@
 import os
 
 import asdf
-from asdf import resolver as res
+from asdf import extension
+
 
 DIRNAME = os.path.dirname(__file__)
 
+# Check if we're running tests from the current directory.
 if DIRNAME == os.path.abspath(os.curdir):
-    # Override the default ASDF url mapping so that all ASDF Standard schemas
+    # Override the ASDF BuiltinExtension so that all ASDF Standard schemas
     # resolve to the ones that are present in this repository, rather than
     # those that are installed with the ASDF python package. This allows for
     # much more direct testing of changes to schemas in this repository.
-    ASDF_SCHEMA_URL_MAPPING = [
+    local_url_mapping = [
         (
             asdf.constants.STSCI_SCHEMA_URI_BASE,
             asdf.util.filepath_to_url(os.path.join(DIRNAME, "schemas", "stsci.edu")) + "/{url_suffix}.yaml",
         )
     ]
-    res.default_url_mapping = res.Resolver(ASDF_SCHEMA_URL_MAPPING, "url")
+
+    class LocalBuiltinExtension(extension.BuiltinExtension):
+        @property
+        def url_mapping(self):
+            return local_url_mapping
+
+    exts = extension.default_extensions.extensions
+    exts.clear()
+    exts.append(LocalBuiltinExtension())
