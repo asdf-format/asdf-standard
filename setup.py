a8f93809c2f23200a93337eb25a2b4654a6a1e7c
William Jamieson <wjamieson@stsci.edu>
2022-02-08 09:58:55 -0500

    Initial changes to enable packaging

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import os
+from pathlib import Path
+from setuptools import setup, find_packages
+
+
+packages = find_packages(where="src")
+packages.append("asdf_standard.resources")
+
+package_dir = {
+    "": "src",
+    "asdf_standard.resources": "resources",
+}
+
+
+def package_yaml_files(directory):
+    paths = sorted(Path(directory).rglob("*.yaml"))
+    return [str(p.relative_to(directory)) for p in paths]
+
+package_data = {
+    "asdf_standard.resources": package_yaml_files("resources"),
+}
+
+setup(
+    packages=packages,
+    package_dir=package_dir,
+    package_data=package_data,
+)
