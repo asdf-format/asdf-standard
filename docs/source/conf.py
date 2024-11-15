4660cb4fccb4f7ab18e9e171a5872b28cf1f64d2
alphasentaurii <rkein@stsci.edu>
2024-11-15 16:47:41 -0500

    use furo theme, draft home page design

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,15 @@ intersphinx_mapping["asdf"] = ("https://asdf.readthedocs.io/en/latest/", None)  
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions += ["sphinx.ext.mathjax", "sphinx.ext.ifconfig", "sphinx.ext.graphviz", "sphinx_asdf"]  # noqa: F405
+extensions += [
+    "sphinx.ext.mathjax", 
+    "sphinx.ext.ifconfig", 
+    "sphinx.ext.graphviz",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.githubpages",
+    "sphinx_asdf",
+]  # noqa: F405
 
 mathjax_path = "https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
 
@@ -75,10 +83,11 @@ source_encoding = "utf-8"
 
 # -- Options for HTML output -------------------------------------------------
 
+html_title = project
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 
 # html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
@@ -86,7 +95,9 @@ html_theme = "sphinx_rtd_theme"
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {}
+html_theme_options = {
+    "sidebar_hide_name": True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -95,12 +106,14 @@ html_static_path = ["_static"]
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
-
+html_additional_pages = {
+    "index": "index.html",
+}
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = "_static/logo.ico"
-html_logo = "_static/logo.png"
+html_favicon = "_static/images/favicon.ico"
+html_logo = "_static/images/logo.png"
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
@@ -125,7 +138,7 @@ html_show_sphinx = True
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
 html_show_copyright = True
 
-html_context = {"bootswatch_css_custom": ["_static/asdfstandard.css"]}
+html_context = {"bootswatch_css_custom": ["_static/custom.css"]}
 
 
 # -- Options for HTMLHelp output ---------------------------------------------
@@ -244,5 +257,5 @@ asdf_schema_reference_mappings = [
 ]
 
 
-def setup(app):
-    app.add_css_file("custom.css")
+# def setup(app):
+#     app.add_css_file("custom.css")
---
+++ docs/source/tutorials/index.rst
@@ -0,0 +1,6 @@
+Tutorials
+=========
+
+Get started using ASDF:
+    - :doc:`Tutorial 1 <1>` - Tutorial1
+    - :doc:`Tutorial 2 <2>` - Tutorial2
