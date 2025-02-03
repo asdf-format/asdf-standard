84f1e9fd3507441cbb596e62f3c8876da3b91c63
alphasentaurii <rkein@stsci.edu>
2025-02-03 12:26:39 -0500

    update asdf subproject url

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,7 @@ subproject_mappings = {
     # Main project
     "asdf-website": ("https://www.asdf-format.org/en/latest/", None),
     # Subprojects
-    "asdf": ("https://www.asdf-format.org/projects/asdf/en/latest/", None),
+    "asdf": ("https://www.asdf-format.org/projects/asdf/en/stable/", None),
     "asdf-coordinates-schemas": ("https://www.asdf-format.org/projects/asdf-coordinates-schemas/en/latest/", None),
     "asdf-transform-schemas": ("https://www.asdf-format.org/projects/asdf-transform-schemas/en/latest/", None),
     "asdf-wcs-schemas": ("https://www.asdf-format.org/projects/asdf-wcs-schemas/en/latest/", None),
@@ -43,10 +43,9 @@ intersphinx_mapping.update(subproject_mappings)  # noqa: F405
 
 # Adds a global navigation in the topbar - consistent across subprojects
 globalnavlinks = {
-    "Projects": "https://www.asdf-format.org",
+    "ASDF Projects": "https://www.asdf-format.org",
     "Tutorials": "https://www.asdf-format.org/en/latest/tutorials/index.html",
     "Community": "https://www.asdf-format.org/en/latest/community/index.html",
-    "Installation": "https://www.asdf-format.org/en/latest/applications/index.html",
 }
 topbanner = ""
 for text, link in globalnavlinks.items():
@@ -134,7 +133,7 @@ html_additional_pages = {}
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = "images/favicon.ico"
+html_favicon = "_static/images/favicon.ico"
 html_logo = ""
 
 # Theme options are theme-specific and customize the look and feel of a theme
